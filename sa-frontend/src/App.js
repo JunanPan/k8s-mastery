2190100b3917bcf2682fc6d19ec0f20c0b654da1
Junan <junanpan@cmu.edu>
2024-02-20 19:23:31 -0800

    change port from 3xxx to 8080

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() { 
-        fetch('http://35.196.210.118:30467/sentiment', {
+        fetch('http://35.196.210.118:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
